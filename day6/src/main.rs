b2b3a544f566d1df19078851f00fb9735732bc54
Trevor Berninger <baladashman@gmail.com>
2017-12-06 11:37:29 -0800

    day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,15 +1,14 @@
 
-fn solve_1(input: &Vec<usize>) -> usize {
+fn solve_1(input: &mut Vec<usize>) -> usize {
     let mut count = 0usize;
-    let mut state = input.clone();
     let mut states: Vec<Vec<usize>> = Vec::new();
 
-    while !states.contains(&state) {
-        states.push(state.clone());
+    while !states.contains(&input) {
+        states.push(input.clone());
         // find largest bank location and size
         let mut bank = 0usize;
         let mut max = 0usize;
-        for (c, x) in state.iter().enumerate() {
+        for (c, x) in input.iter().enumerate() {
             if *x > max {
                 bank = c;
                 max = *x;
@@ -17,13 +16,13 @@ fn solve_1(input: &Vec<usize>) -> usize {
         }
         // redistribute
         let mut i = max;
-        state[bank] = 0;
+        input[bank] = 0;
         while i > 0 {
             bank += 1;
             if bank >= input.len() {
                 bank = 0;
             }
-            state[bank] += 1;
+            input[bank] += 1;
             i -= 1;
         }
         count += 1;
@@ -32,8 +31,10 @@ fn solve_1(input: &Vec<usize>) -> usize {
 }
 
 fn main() {
-    let input: Vec<usize> = vec![11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11];
-    let test_input: Vec<usize> = vec![0,2,7,0];
-    println!("{}", solve_1(&test_input));
-    println!("{}", solve_1(&input)); // 100 is wrong!
+    let mut input: Vec<usize> = vec![11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11];
+    let mut test_input: Vec<usize> = vec![0,2,7,0];
+    println!("{}", solve_1(&mut test_input));
+    println!("{}", solve_1(&mut test_input));
+    println!("{}", solve_1(&mut input));
+    println!("{}", solve_1(&mut input));
 }
