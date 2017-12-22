4d8aeb8c287c748de28623dcd328f00a9ef1978a
Delamare2112 <baladashman@gmail.com>
2017-12-22 08:50:28 -0800

    day 15 part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,10 +1,16 @@
 struct Generator {
     previous: usize,
-    factor: usize
+    factor: usize,
+    criteria: usize
 }
 impl Generator {
     fn next(&mut self) -> usize {
-        self.previous = (self.previous * self.factor) % 2147483647;
+        loop {
+            self.previous = (self.previous * self.factor) % 2147483647;
+            if self.previous % self.criteria == 0 {
+                break;
+            }
+        }
         self.previous
     }
 }
@@ -20,8 +26,11 @@ fn judge(ga: &mut Generator, gb: &mut Generator, count: usize) -> usize {
 }
 
 fn main() {
-    let mut ga = Generator {factor: 16807, previous: 591};
-    let mut gb = Generator {factor: 48271, previous: 393};
+    let mut ga = Generator {factor: 16807, previous: 591, criteria: 1};
+    let mut gb = Generator {factor: 48271, previous: 393, criteria: 1};
+    let mut ga_2 = Generator {factor: 16807, previous: 591, criteria: 4};
+    let mut gb_2 = Generator {factor: 48271, previous: 393, criteria: 8};
 
     println!("judge: {}", judge(&mut ga, &mut gb, 40000000));
+    println!("judge: {}", judge(&mut ga_2, &mut gb_2, 5000000));
 }
